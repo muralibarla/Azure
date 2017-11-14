a5be8a77ab16d7b4b46620f5a7701f9d89224fee
Murali Barla <murali.barla@nuance.com>
2017-11-13 22:34:42 -0500

    some changes

--- java-tomcat-install.sh
+++ java-tomcat-install.sh
@@ -1,12 +1,12 @@
 ﻿
 # Install Java
 sudo yum install -y updates
-sudo yum install -y java-1.7.0-openjdk
+sudo yum install -y $1
 
 # Install Tomcat
-sudo wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz
-tar -xzvf  apache-tomcat-8.5.23.tar.gz 
-sudo mv apache-tomcat-8.5.23 /srv/
-sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat
-sudo chown -R neurostar:neurostar /srv/tomcat
+wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz 
+tar -xzvf  apache-tomcat-8.5.23.tar.gz  
+sudo mv apache-tomcat-8.5.23 /srv/ 
+sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat 
+sudo chown -R neurostar:neurostar /srv/tomcat 
 
