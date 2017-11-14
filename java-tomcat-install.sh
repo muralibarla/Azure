85d738cf06b707e2ec5c037d54a8cc1600ca6f91
Murali Barla <murali.barla@nuance.com>
2017-11-13 23:05:49 -0500

    newline characters at the end of line 7

--- java-tomcat-install.sh
+++ java-tomcat-install.sh
@@ -4,8 +4,8 @@ sudo yum install -y updates
 sudo yum install -y $1
 
 # Install Tomcat
-sudo wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz 
-tar -xzvf  apache-tomcat-8.5.23.tar.gz  
+sudo wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz
+tar -xzvf  apache-tomcat-8.5.23.tar.gz 
 sudo mv apache-tomcat-8.5.23 /srv/ 
 sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat 
 sudo chown -R neurostar:neurostar /srv/tomcat 
