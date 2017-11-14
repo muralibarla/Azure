325f5e5e281fb11224acee236eeb6ed615f6d5f9
Murali Barla <murali.barla@nuance.com>
2017-11-13 22:40:49 -0500

    Adding sudo

--- java-tomcat-install.sh
+++ java-tomcat-install.sh
@@ -4,7 +4,7 @@ sudo yum install -y updates
 sudo yum install -y $1
 
 # Install Tomcat
-wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz 
+sudo wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz 
 tar -xzvf  apache-tomcat-8.5.23.tar.gz  
 sudo mv apache-tomcat-8.5.23 /srv/ 
 sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat 
