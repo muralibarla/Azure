287788e678683b7f71674e6538b82cdbfdfb58c2
Murali Barla <murali.barla@nuance.com>
2017-11-14 16:28:47 -0500

    getting customized tomcat

--- java-tomcat-install.sh
+++ java-tomcat-install.sh
@@ -5,9 +5,16 @@ sudo yum install -y java-1.7.0-openjdk
 
 # Install Tomcat
 sudo yum install wget
-sudo wget http://apache.osuosl.org/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz 
-sudo tar -xzvf  apache-tomcat-8.5.23.tar.gz 
+sudo wget https://www.dropbox.com/s/wh9wz41rrtw90eq/apache-tomcat-8.5.23.zip 
+sudo unzip apache-tomcat-8.5.23.zip 
 sudo mv apache-tomcat-8.5.23 /srv/ 
 sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat 
 sudo chown -R neurostar:neurostar /srv/tomcat 
+
+sudo sh /srv/tomcat/bin/startup.sh
+
+if netstat -tulpen | grep 8080
+then
+	exit 0
+fi
 
