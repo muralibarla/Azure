1f9e46d9bdbddc88a3e682510e7f4935c8908505
Murali Barla <murali.barla@nuance.com>
2017-11-14 16:45:55 -0500

    updating some commands

--- java-tomcat-install.sh
+++ java-tomcat-install.sh
@@ -5,13 +5,13 @@ sudo yum install -y java-1.7.0-openjdk
 
 # Install Tomcat
 sudo yum install wget
-sudo wget https://www.dropbox.com/s/wh9wz41rrtw90eq/apache-tomcat-8.5.23.zip 
+sudo wget https://www.dropbox.com/s/mgjaixl3vlhd0vk/apache-tomcat-8.5.23.zip 
 sudo unzip apache-tomcat-8.5.23.zip 
 sudo mv apache-tomcat-8.5.23 /srv/ 
 sudo ln -s /srv/apache-tomcat-8.5.23 /srv/tomcat 
-sudo chown -R neurostar:neurostar /srv/tomcat 
+sudo chown -R neurostar:neurostar /srv/tomcat/
 
-sudo sh /srv/tomcat/bin/startup.sh
+sh /srv/tomcat/bin/startup.sh
 
 if netstat -tulpen | grep 8080
 then
