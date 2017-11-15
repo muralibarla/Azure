f040ac8eb47260ccb60434d96fefec3441edb25b
Murali Barla <murali.barla@nuance.com>
2017-11-14 21:11:12 -0500

    ServiceMix 4.5.2 installation

---
+++ java-servicemix-install.sh
@@ -0,0 +1,10 @@
+# Install Java
+sudo yum install -y updates
+sudo yum install -y java-1.7.0-openjdk
+
+# Install ServiceMix
+sudo wget http://archive.apache.org/dist/servicemix/servicemix-4/4.5.2/apache-servicemix-4.5.2.tar.gz
+sudo tar -xzvf apache-servicemix-4.5.2.tar.gz
+sudo mv apache-servicemix-4.5.2 /srv
+sudo ln -s /srv/apache-servicemix-4.5.2 /srv/servicemix
+sudo chown -R neurostar:neurostar /srv/servicemix/
