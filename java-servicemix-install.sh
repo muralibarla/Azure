1e7fa291094d86ed070c5de2379971a83aa2ed54
Murali Barla <murali.barla@nuance.com>
2017-12-18 09:26:28 -0500

    Removing service creation

--- java-servicemix-install.sh
+++ java-servicemix-install.sh
@@ -1,6 +1,12 @@
 # Install Java
 sudo yum install -y updates
 sudo yum install -y java-1.7.0-openjdk
+sudo echo "JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:bin/java::")" | sudo tee -a /etc/profile
+source /etc/profile
+
+# Setting Time Zone to EST
+sudo mv /etc/localtime /etc/localtime.bak
+sudo ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 
 # Install ServiceMix
 sudo wget http://archive.apache.org/dist/servicemix/servicemix-4/4.5.2/apache-servicemix-4.5.2.tar.gz
@@ -12,34 +18,3 @@ sudo chown -R neurostar:neurostar /srv/servicemix/
 sudo chmod -R 777 /srv/servicemix
 sudo chmod -R 777 /srv/servicemix/
 
-# Setting Time Zone to EST
-sudo mv /etc/localtime /etc/localtime.bak
-sudo ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
-
-# Creating ServiceMix service 
-sudo cat > /etc/systemd/system/servicemix.service << EOF1
-
-[Unit]
-Description=ServiceMix service
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/srv/servicemix/bin/start
-ExecStop=/srv/servicemix/bin/stop
-User=neurostar
-Group=neurostar
-Restart=always
-RestartSec=9
-StandardOutput=syslog
-StandardError=syslog
-SyslogIdentifier=servicemix
-
-[Install]
-WantedBy=multi-user.target
-
-EOF1
-
-sudo systemctl daemon-reload
-sudo systemctl start servicemix
-sudo systemctl enable servicemix
